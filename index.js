575ff9854e727cf0f89da35d90c051abc38405ae
Nidhi Jikadra <nidhijikadra3103@gmail.com>
2023-09-15 14:27:42 +0530

    Done

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-const http = require ('http')
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req,res)=>{
-    if(req.url=='/'){
-        res.end("welcome")
-    }
+  
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    fs.readFile("home.html", (err, data) => {
+      if (err) {
+        console.log("Error on this Page.");
+        res.end("Error");
+      } 
+      else {
+        res.end(data);
+      }
+    });
+  }
+ else if (req.url == "/login") {
+    fs.readFile("login.html", (err, data) => {
+      if (err) {
+        console.log("Error on this Page.");
+        res.end("Error");
+      } 
+      else {
+        res.end(data);
+      }
+    });
+  }
+ else if (req.url == "/signup") {
+    fs.readFile("signup.html", (err, data) => {
+      if (err) {
+        console.log("Error on this Page.");
+        res.end("Error");
+      } 
+      else {
+        res.end(data);
+      }
+    });
+  }
+ else if (req.url == "/about") {
+    fs.readFile("about.html", (err, data) => {
+      if (err) {
+        console.log("Error on this Page.");
+        res.end("Error");
+      } 
+      else {
+        res.end(data);
+      }
+    });
+  }
+  else if (req.url == "/blog") {
+    fs.readFile("blog.html", (err, data) => {
+      if (err) {
+        console.log("Error on this Page.");
+        res.end("Error");
+      } 
+      else {
+        res.end(data);
+      }
+    });
+  }
+  else{
+    res.end("Invalid Page")
+  }
 })
 
-server.listen(8090,()=>{
+server.listen(8090, () => {
     console.log("Server is running..");
-})
+  });
